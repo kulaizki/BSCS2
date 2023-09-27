0636fe358fb400a0cec266d5da9f84054d0a775e
kulaizki <kulaizke@gmail.com>
2023-09-27 18:54:20 +0800

    LL review

---
+++ Data Structures and Algorithms/General/test.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node *link;
+} *LIST;
+
+void insertLast(LIST *L, int elem);
+void displayList(LIST L);
+void deleteFirst(LIST *L);
+void deleteLast(LIST *L);
+void deleteAllOccurences(LIST *L, int elem);
+
+int main() {
+
+    LIST L = NULL;
+
+    insertLast(&L, 1);
+    insertLast(&L, 2);
+    insertLast(&L, 3);
+    insertLast(&L, 4);
+    insertLast(&L, 4);
+    insertLast(&L, 4);
+    insertLast(&L, 4);
+    insertLast(&L, 5);
+    insertLast(&L, 5);
+
+    deleteFirst(&L);
+    deleteLast(&L);
+    deleteAllOccurences(&L, 4);
+
+    displayList(L);
+
+}
+
+void insertLast(LIST *L, int elem) {
+
+    LIST *trav;
+    for (trav = L; *trav != NULL; trav = &(*trav)->link) {}
+    LIST temp = (LIST)malloc(sizeof(struct node));
+    temp->data = elem;
+    temp->link = NULL;
+    *trav = temp;
+}
+
+void displayList(LIST L) {
+
+    while (L != NULL) {
+        printf("%d ", L->data);
+        L = L->link;
+    }
+    printf("\n");
+}
+
+void deleteFirst(LIST *L) {
+
+    if (*L != NULL) {
+        LIST temp = *L;
+        *L = (*L)->link;
+        free(temp);
+    }
+}
+
+void deleteLast(LIST *L) {
+
+    if (*L != NULL) {
+        LIST *trav, temp;
+        for (trav = L; (*trav)->link->link != NULL; trav = &(*trav)->link) {}
+        temp = (*trav)->link;
+        (*trav)->link = NULL;
+        free(temp);
+    }
+}
+
+void deleteAllOccurences(LIST *L, int elem) {
+
+    if (*L != NULL) {
+        LIST *trav = L;
+        while (*trav != NULL) {
+            if ((*trav)->data == elem) {
+                LIST temp = *trav;
+                *trav = (*trav)->link;
+                free(temp);
+            } else {
+                trav = &(*trav)->link;
+            }
+        }
+    }
+}
