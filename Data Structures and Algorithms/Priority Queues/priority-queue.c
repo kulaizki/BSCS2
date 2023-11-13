cbd39b2ee066affe19b8da6673a9b1834471b9e5
kulaizki <kulaizke@gmail.com>
2023-11-13 23:25:12 +0800

    ross pqueue

--- Data Structures and Algorithms/Priority Queues/priority-queue.c
+++ Data Structures and Algorithms/Priority Queues/priority-queue.c
@@ -1,161 +1,124 @@
-#include "_header.h"
+#include <stdio.h>
+#include <stdlib.h>
+ 
+#define MAX 20
+ 
+typedef struct {
+    int heap[MAX];
+    int last;
+} queueType, *pQueue;
+ 
+void initQueue(pQueue q);
+void displayHeap(pQueue q, int index, int level);
+ 
+void insertMax(pQueue q, int elem);
+void deleteMax(pQueue q);
+void heapifyMax(int arr[], int index, int last);
+    
+void heapSort(int arr[], int size);
+ 
+int main() {
 
-// * FUNCTION DEFINITIONS HERE
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
+    queueType myQueue;
+    pQueue q = &myQueue;
 
-void Heapify(pQueue *q, int index) {
-    int left = LeftChild(index);
-    int right = RightChild(index);  
-    int smallest = index;
+    initQueue(q);
+    insertMax(q, 10);
+    insertMax(q, 20);
+    insertMax(q, 15);
+    insertMax(q, 30);
+    insertMax(q, 40);
+    insertMax(q, 50);
+    deleteMax(q);
+    displayHeap(q, 0, 0);
+ 
+    int arr[] = {1, 6, 3, 9, 4, 69, 13, 12};
+    heapSort(arr, 8);
 
-    if(left <= q->lastIndex && q->heap[left] < q->heap[smallest]) {
-        smallest = left;
-    }
-
-    if(right <= q->lastIndex && q->heap[right] < q->heap[smallest]) {
-        smallest = right;
-    }
-
-    if(smallest != index) {
-        int temp = q->heap[index];
-        q->heap[index] = q->heap[smallest];
-        q->heap[smallest] = temp;
-        Heapify(q, smallest);
-    }
-}
-
-void Insert(int elem, pQueue *q){
-  int temp, index;
-  q->heap[++q->lastIndex] = elem;
-  for(index = q->lastIndex; q->heap[index] < q->heap[(index-1)/2]; index = (index-1)/2){
-    swap(&q->heap[index], &q->heap[(index-1)/2]);
-  }
-}
-
-void Display(pQueue q){
-    int x, y;
-    for(x = 0; x <= q.lastIndex; x++){
-       printf("[%d] => ", q.heap[x]);
-
-       if(LeftChild(x) <= q.lastIndex) {
-           printf("%d => ", q.heap[(x * 2) + 1]);
-           if(RightChild(x) <= q.lastIndex) printf("%d", q.heap[(x * 2) + 2]);       
-        }
-
-       printf("\n");
-    } 
-}
-
-int LeftChild(int index) {
-    return (index * 2) + 1;
-}
-
-int RightChild(int index) {
-    return (index * 2) + 2;
-}
-
-int SmallestChild(pQueue q, int index) {
-    int left = LeftChild(index); 
-    int right = RightChild(index);
-
-    return q.heap[left] < q.heap[right] ? left : right;
-}
-
-int DeleteMin(pQueue *q) {
-    // int temp, index = 0, retVal = q->heap[index], isSmallest = 0;
-    // q->heap[index] = q->heap[q->lastIndex];
-    // q->heap[q->lastIndex--] = DNE;
-    // while(q->heap[index] > SmallestChild(*q, index) && (index * 2) - 1 <= q->lastIndex) {
-    //     int left = LeftChild(index), right = RightChild(index), temp = q->heap[index];
-
-    //     if(SmallestChild(*q, index) == left) {
-    //         q->heap[index] = q->heap[(index *2) - 1];
-    //         q->heap[(index * 2) - 1] = temp;
-    //         index = (index * 2) - 1;
-    //     }
-
-    //     if(SmallestChild(*q, index) == right) {
-    //         q->heap[index] = q->heap[(index*2) + 2];
-    //         q->heap[(index*2) + 2]= temp;
-    //         index = (index*2) + 2;
-    //     }
-    // }
-    // return retVal;
-
-    int retval = q->heap[0];
-    q->heap[0] = q->heap[q->lastIndex--];
-    Heapify(q, 0);
-    return retval;
-}
-
-void Initialize(pQueue *q) {
-    int x;
-    for(x = 0; x < MAX; x++) {
-        q->heap[x] = DNE;
-    }
-    q->lastIndex = -1;
-}
-
-void MakeNull(pQueue *q){
-    int x;
-    for(x = 0; x <= q->lastIndex; x++){
-        q->heap[x] = DNE;
-    }
-}
-
-//heap sort psuedo
-// turn an array into a heap
-// deletemin from your heap
-// store it in an array
-
-void heapsort(int *arr, int size) {
-    pQueue q;
-    Initialize(&q);
-    int x;
-    for(x = 0; x < size; x++) {
-        Insert(arr[x], &q);
-    }
-
-    for(x=0;x < size ;x++){
-        arr[x] = DeleteMin(&q);
-    }
-}
-
-int main(void)  {   
-
-    int arr [9]= {9,2,18,23,5,1,8,27,7};
-    pQueue q;
-
-    // Initialize(&q);
-    // Insert(3, &q);
-    // Insert(10, &q);
-    // Insert(2, &q);
-    // Insert(39, &q);
-    // Insert(9, &q);
-    // Insert(15, &q);
-    // Insert(23, &q);
-    // Insert(4, &q);
-    // Insert(1, &q);
-    // Insert(20, &q);
-    // Insert(7, &q);
-    // Insert(15, &q);
-    // Insert(0, &q);
-    // DeleteMin(&q);
-    // DeleteMin(&q);
-    // DeleteMin(&q);
-    // DeleteMin(&q);
-    // // DeleteMin(&q);
-    // // DeleteMin(&q);
-    // Display(q);
-
-    heapsort(arr, 9);
-    for(int x = 0; x < 9; x ++) {
+    for (int x = 0; x < 8; x++) {
         printf("%d ", arr[x]);
     }
-    
-    return 0;
+
+    printf("\n");
+}
+ 
+void initQueue(pQueue q) {
+
+    q->last = -1;
+}
+ 
+void insertMax(pQueue q, int elem) {
+
+    if (q->last < MAX - 1) {
+        int x, temp;
+        q->heap[++q->last] = elem;
+        for (x = q->last; q->heap[x] > q->heap[(x - 1) / 2]; x = (x - 1) / 2) {
+            temp = q->heap[x];
+            q->heap[x] = q->heap[(x - 1) / 2];
+            q->heap[(x - 1) / 2] = temp;
+        }
+    } else {
+        printf("Heap is full\n");
+    }
+}
+ 
+void deleteMax(pQueue q) {
+
+    if (q->last > -1)
+    {
+        int lastIndex = q->last;
+        int temp = q->heap[0];
+        q->heap[0] = q->heap[q->last];
+        q->heap[q->last--] = temp;
+        heapifyMax(q->heap, 0, lastIndex);
+    } else {
+        printf("There's nothing to Delete\n");
+    }
+}
+ 
+void heapifyMax(int arr[], int index, int last) {
+
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+    int largest = index;
+
+    if (left <= last && arr[largest] < arr[left]) {
+        largest = left;
+    }
+    if (right <= last && arr[largest] < arr[right]) {
+        largest = right;
+    }
+    if (largest != index) {
+        int temp = arr[index];
+        arr[index] = arr[largest];
+        arr[largest] = temp;
+        heapifyMax(arr, largest, --last);
+    }
+}
+ 
+void heapSort(int arr[], int size) {
+
+    for (int x = size / 2 - 1; x >= 0; x--) {
+        heapifyMax(arr, x, size);
+    }
+    for (int x = size - 1; x > 0; x--) {
+        int temp = arr[0];
+        arr[0] = arr[x];
+        arr[x] = temp;
+        heapifyMax(arr, 0, x - 1);
+    }
+}
+ 
+void displayHeap(pQueue q, int index, int level) {
+
+    if (q == NULL || index > q->last) {
+        return;
+    }
+ 
+    displayHeap(q, 2 * index + 2, level + 1); // Right subtree
+    for (int i = 0; i < level; i++) {
+        printf("     "); // Adjust spacing for tree visualization
+    }
+    printf("%d\n", q->heap[index]);           // Print the current element
+    displayHeap(q, 2 * index + 1, level + 1); // Left subtree
 }
