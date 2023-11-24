635b4dc17cb7a62ff902371e21cbb8f8dfbcd5fb
kulaizki <kulaizke@gmail.com>
2023-11-25 04:17:34 +0800

    create, insert, min, max, display

---
+++ Data Structures and Algorithms/Review/BST-scratch.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum { FALSE, TRUE } Boolean;
+
+typedef struct node {
+    int e;
+    struct node *LC;
+    struct node *RC;
+} *BST;
+
+BST createNode(int e);
+void insert(BST *B, int e);
+void insertIterative(BST *B, int e);
+void deleteIterative(BST *B, int e);
+int min(BST B);
+int minIterative(BST B);
+int max(BST B);
+int maxIterative(BST B);    
+int sum (BST B);
+Boolean search(BST B);
+Boolean searchIterative(BST B);
+void displayPreOrder(BST B);
+void displayInOrder(BST B);
+void displayPostOrder(BST B);
+
+int main() {
+
+    BST gdsc = NULL;
+
+    insert(&gdsc, 1);
+    insert(&gdsc, 2);
+    insert(&gdsc, 2);
+    insert(&gdsc, 3);
+
+    printf("PreOrder\n");
+    displayPreOrder(gdsc);
+    printf("\nInOrder\n");
+    displayInOrder(gdsc);
+    printf("\nPostOrder\n");
+    displayPostOrder(gdsc);
+
+    printf("\nmin: %d\n", min(gdsc));
+    printf("max: %d\n", max(gdsc));
+}
+
+BST createNode(int e) {
+
+    BST newNode = (BST)malloc(sizeof(struct node));
+    newNode->e = e;
+    newNode->LC = NULL;
+    newNode->RC = NULL;
+
+    return newNode;
+}
+
+void insert(BST *B, int e) {
+
+    if (*B == NULL) {
+        *B = createNode(e);
+    } else if (e < (*B)->e) {
+        insert(&(*B)->LC, e);
+    } else if (e > (*B)->e) {
+        insert(&(*B)->RC, e);
+    }
+
+    return;
+}
+
+void insertIterative(BST *B, int e) {
+
+}
+
+void deleteIterative(BST *B, int e) {
+
+}
+
+int min(BST B) {
+
+    return (B != NULL) ? (B->LC == NULL) ? B->e : min(B->LC) : -1;
+}
+
+int minIterative(BST B) {
+
+}
+
+int max(BST B) {
+
+    return (B != NULL) ? (B->RC == NULL) ? B->e : max(B->RC) : -1;
+}
+
+int maxIterative(BST B) {
+
+}
+
+int sum (BST B) {
+
+    return (B == NULL) ? 0 : B->e + sum(B->LC) + sum(B->RC);
+}
+
+Boolean search(BST B) {
+
+}
+
+Boolean searchIterative(BST B) {
+
+}
+
+void displayPreOrder(BST B) {
+
+    if (B != NULL) {
+        printf("%d ", B->e);
+        if (B->LC != NULL) displayPreOrder(B->LC);
+        if (B->RC != NULL) displayPreOrder(B->RC);
+    } else {
+        printf("BST is empty");
+    }
+}
+
+void displayInOrder(BST B) {
+
+    if (B != NULL) {
+        printf("%d ", B->e);
+        if (B->LC != NULL) displayInOrder(B->LC);
+        if (B->RC != NULL) displayInOrder(B->RC);
+    } else {
+        printf("BST is empty");
+    }
+}
+
+void displayPostOrder(BST B) {
+
+    if (B != NULL) {
+        printf("%d ", B->e);
+        if (B->LC != NULL) displayPostOrder(B->LC);
+        if (B->RC != NULL) displayPostOrder(B->RC);
+    } else {
+        printf("BST is empty");
+    }
+}
--- Data Structures and Algorithms/Review/tempCodeRunnerFile.c
+++
@@ -1,3 +0,0 @@
-	for(x = 0; x < VH_SIZE; x++) {
-		printf("%8d%11s%12d%15d\n", x, V.VH_node[x].elem.prodID, V.VH_node[x].LC, V.VH_node[x].RC);
-	}
